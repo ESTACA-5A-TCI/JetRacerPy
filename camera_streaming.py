c1eb96deb8e07931681ed4623ac2abeaa3cb834d
FouadKhenfri <fouad.khenfri@gmail.com>
2024-12-08 21:04:30 +0100

    update file

--- camera_streaming.py
+++ camera_streaming.py
@@ -10,12 +10,11 @@ class RTSPServer:
     def __init__(self):
         self.server = GstRtspServer.RTSPServer()
         self.factory = GstRtspServer.RTSPMediaFactory()
-
         self.factory.set_launch((
             "nvarguscamerasrc ! video/x-raw(memory:NVMM),width=1280,height=720,framerate=30/1 "
-            "! omxh264enc ! rtph264pay name=pay0 pt=96"
+            "! nvv4l2h264enc iframeinterval=15 control-rate=1 bitrate=4000000 preset=low-latency-hp "
+            "! h264parse ! rtph264pay config-interval=1 name=pay0 pt=96"
         ))
-
         self.factory.set_shared(True)
         self.server.get_mount_points().add_factory("/test", self.factory)
         self.server.attach(None)
